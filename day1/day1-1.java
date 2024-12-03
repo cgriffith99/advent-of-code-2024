d278e490f07df51d23e45d09c1f982358746cf12
raptor0999 <raptordev@proton.me>
2024-12-03 08:29:38 -0500

    Initial commit

---
+++ day1/day1-1.java
@@ -0,0 +1,48 @@
+package myadventcode;
+
+import java.util.Collections;
+import java.util.ArrayList;
+import java.util.List;
+import java.nio.file.*;
+import java.lang.Exception;
+import java.lang.Math;
+
+
+public class AdventOfCode {
+	public static void main(String[] args) {
+		List<String> lines = new ArrayList<String>();
+
+		// read input data in
+		try {
+			lines = Files.readAllLines(Paths.get("day1-input"));
+		} catch(Exception e) {
+
+		}
+		
+		// setup lists to hold the numbers
+		List<Integer> firstList = new ArrayList<Integer>();
+		List<Integer> secondList = new ArrayList<Integer>();
+
+		// iterate over and split the lines out and add numbers to lists
+		for(String l: lines) {
+			String[] numbers = l.split("   ");
+			firstList.add(Integer.parseInt(numbers[0]));
+			secondList.add(Integer.parseInt(numbers[1]));
+		}
+
+		// sort lists in ascending order
+		Collections.sort(firstList);
+		Collections.sort(secondList);
+
+		Integer totalSum = 0;
+
+		// iterate over both lists at same time and find the difference between each set and add to sum
+		for(Integer i=0;i<firstList.size();i++) {
+			totalSum += Math.abs(firstList.get(i) - secondList.get(i));
+		}
+
+		// spit out the sum of the differences for answer
+		System.out.println("Answer for Day 1 Star 1: " + totalSum);
+	}
+}
+
